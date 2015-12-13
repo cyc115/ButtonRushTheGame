8b9ca86543b955a633dc3e7cf497297d214c556e
Faraz Oman <faraz@farazoman.com>
2015-12-12 19:45:30 -0500

    added a mock function of client side calculations
    again not tested exactly because I dont get the things but the theory is
    fine, I drew pictures to help prove to myself

--- Server/server/calibrate.js
+++ Server/server/calibrate.js
@@ -6,3 +6,23 @@ var calibrateClock = function(iButtonClicked, socket){
 	io.to(currentSocketId).emit("server_timestamps", {incoming : incoming_, outgoing : outgoing_});
 	//all the calculations are done client side
 }
+
+var threshold = 5; //in miliseconds
+//A mock function for how the client does the calculations
+var computeOffset = function(){
+	offset = 0;
+
+	//this while loop will get a good average of the latency by repeating until a threshold is met
+	do{
+
+		var outgoing_c = new Date().getTime() + offset;
+		//call calibrate clock remotely and get variables
+		var incoming_c = new Date().getTime() - offset;
+		//do this calculation: calc = ((incoming_s - outgoing_c) + (outgoing_s - incoming_c)) / 2
+		// more calcs : offset = offset + calc;
+
+	}while(Math.abs(offset) > threshold);
+
+	return offset;
+
+}
